580283ba64fdf8a5fe9e08f7ec066f33c3824456
Jack McGrath <eejdm@leeds.ac.uk>
2023-04-25 13:39:04 +0100

    Add explanation

--- prep_mtex.m
+++ prep_mtex.m
@@ -1,14 +1,21 @@
-function prep_mtex(version, SGPpath)
+function prep_mtex(SGPpath, mtex_version)
+% Function to prepare mtex and set up the correct paths. Edit the default
+% path to the SGPeizometery software. Adding this script to your default
+% MATLAB search path (or at softlinking to it) will mean that this script
+% will be easily searchable, and can be launched from your data folder
 
-% Set up mtex version and path variables
+% Jack McGrath, 2023, Uni of Leeds
+
+
+% Set up path and mtex version variables
 if nargin < 1
-    mtex_version = '5.1.1SGP';
-else
-    mtex_version = version;
+    SGPpath = '/nfs/a285/homes/eejdm/software/SGPiezometry';
 end
 
 if nargin < 2
-    SGPpath = '/nfs/a285/homes/eejdm/software/SGPiezometry';
+    mtex_version = '5.1.1SGP';
+else
+    mtex_version = version;
 end
 
 curdir = pwd;
@@ -28,7 +35,7 @@ install_mtex
 % Add mtex and SGP to path (mtex normally added in installation, but this
 % sometimes fails
 addpath(genpath(mtex))
-addpath('/nfs/a285/homes/eejdm/software/SGPiezometry')
+addpath('SGPpath')
 
 % Turn name conflict warning back on
 warning('on', 'MATLAB:dispatcher:nameConflict')
